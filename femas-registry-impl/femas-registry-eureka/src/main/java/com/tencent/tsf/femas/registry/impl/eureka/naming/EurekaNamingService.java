f237c6d9a1600168ef53eca086f27b6c6f0d4abe
yuanrengu <heyonggang0811@126.com>
2022-03-28 14:20:57 +0800

    规范化

--- femas-registry-impl/femas-registry-eureka/src/main/java/com/tencent/tsf/femas/registry/impl/eureka/naming/EurekaNamingService.java
+++ femas-registry-impl/femas-registry-eureka/src/main/java/com/tencent/tsf/femas/registry/impl/eureka/naming/EurekaNamingService.java
@@ -1,4 +1,4 @@
-package com.tencent.tsf.femas.registry.impl.eureka.naming;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
  * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
  * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
@@ -10,6 +10,7 @@ package com.tencent.tsf.femas.registry.impl.eureka.naming;/*
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+package com.tencent.tsf.femas.registry.impl.eureka.naming;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.shared.Application;
